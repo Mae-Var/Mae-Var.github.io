eade2061dd4b68a42efb94d9eb9cd70a1cf043f2
B Popov <ashelan@gmail.com>
2019-08-08 14:22:11 +0300

    updt

--- contactform.js
+++ contactform.js
@@ -1,18 +1,85 @@
-function validate() {
-  var name = document.getElementById('name');
-  var subject = document.getElementById('subject');
-  var email = document.getElementById('email');
-  var message = document.getElementById('message');
+//update this with your js_form selector
+var form_id_js = 'javascript_form';
 
-  if (
-    name.value.trim() == '' ||
-    subject.length >= 2 ||
-    email.value.trim() == '' ||
-    message.length >= 10
-  ) {
-    alert('Please, fill in all fields.');
-    return false;
-  } else {
-    true;
-  }
+var data_js = {
+  access_token: '{tjyf1wnc3m70265k0h15op30}' // sent after you sign up
+};
+
+// function js_onSuccess() {
+//   // remove this to avoid redirect
+//   window.location =
+//     window.location.pathname + '?message=Email+Successfully+Sent%21&isError=0';
+// }
+
+// function js_onError(error) {
+//   // remove this to avoid redirect
+//   window.location =
+//     window.location.pathname + '?message=Email+could+not+be+sent.&isError=1';
+// }
+
+var sendButton = document.getElementById('js_send');
+
+function js_send() {
+  sendButton.value = 'Sending…';
+  sendButton.disabled = true;
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function() {
+    if (request.readyState == 4 && request.status == 200) {
+      js_onSuccess();
+    } else if (request.readyState == 4) {
+      js_onError(request.response);
+    }
+  };
+
+  var subject = document.querySelector('#' + form_id_js + " [name='subject']")
+    .value;
+  var message = document.querySelector('#' + form_id_js + " [name='text']")
+    .value;
+  data_js['subject'] = subject;
+  data_js['text'] = message;
+  var params = toParams(data_js);
+
+  request.open('POST', 'https://postmail.invotes.com/send', true);
+  request.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+  request.send(params);
+
+  return false;
 }
+
+sendButton.onclick = js_send;
+
+function toParams(data_js) {
+  var form_data = [];
+  for (var key in data_js) {
+    form_data.push(
+      encodeURIComponent(key) + '=' + encodeURIComponent(data_js[key])
+    );
+  }
+
+  return form_data.join('&');
+}
+
+var js_form = document.getElementById(form_id_js);
+js_form.addEventListener('submit', function(e) {
+  e.preventDefault();
+});
+
+// function validate() {
+//   var name = document.getElementById('name');
+//   var subject = document.getElementById('subject');
+//   var email = document.getElementById('email');
+//   var message = document.getElementById('message');
+
+//   if (
+//     name.value.trim() == '' ||
+//     subject.length >= 2 ||
+//     email.value.trim() == '' ||
+//     message.length >= 10
+//   ) {
+//     alert('Please, fill in all fields.');
+//     return false;
+//   } else {
+//     true;
+//   }
+// }
